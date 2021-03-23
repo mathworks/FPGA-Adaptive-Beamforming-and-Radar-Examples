85633a729c7f2f707dbc09f4ffc0d189d72289f2
Tom Mealey <tmealey@mathworks.com>
2021-03-23 11:01:25 -0400

    Add prebuilt bitstream to ZCU111 demo directory

--- zcu111_mvdr_demo/program_board.m
+++ zcu111_mvdr_demo/program_board.m
@@ -1,6 +1,6 @@
 % program_board Utility script to program the board after bitstream generation
 
-bitstream = 'hdl_prj\vivado_ip_prj\vivado_prj.runs\impl_1\system_wrapper.bit';
+bitstream = fullfile('binaries','zcu111_mvdr.bit');
 devicetree = 'devicetree_adi_axistream_32.dtb';
 rdName = 'I/Q';
 
@@ -8,7 +8,7 @@ rdName = 'I/Q';
 z = zynq;
 
 % Upload customer devicetree to board
-z.putFile(devicetree,'/mnt');
+z.putFile(fullfile('binaries',devicetree),'/mnt');
 
 % Program the board
 ZynqRF.common.internal.downloadBitstreamToRFSoC(z,bitstream,devicetree,rdName,{});
